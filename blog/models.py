b189ca36c9c4eb27b17602633f2b6a8ccdff4068
Hafiz Manfaz Ali <manfaz050@gmail.com>
2025-08-06 21:57:02 +0500

    binding tag a third party app to the post for tagging

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 # from django.db.models.functions import Now
 from django.conf import settings
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -29,6 +30,7 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+    tags       = TaggableManager()
 
 
     class Meta:
