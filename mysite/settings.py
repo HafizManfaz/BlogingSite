8f64986133501c1a860bc0f68dc5f9e2ee5014c3
Hafiz Manfaz Ali <manfaz050@gmail.com>
2025-08-31 23:36:07 +0500

    markdown to html

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'taggit',
     'blog.apps.BlogConfig',
